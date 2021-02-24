424d10778e1cf041bdd1ff52985eb4c084a3d76b
Heisenberg <nancologist@gmail.com>
2021-02-24 21:31:20 +0100

    Outsource Squares cmp.

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
+import Squares from '../components/squres/Squares';
 import './App.css';
-import { texts } from '../data/data';
-import Square from '../components/square/Square';
 
 function App() {
   return (
     <div className="App">
       <h1>Let's play Vingo!</h1>
-      <div className="squares">
-        {texts.map((text, index) => {
-          const x = index % 5;
-          const y = Math.floor( index / 5 );
-          return <Square text={text} x={x} y={y} key={index} />;
-        })}
-      </div>
+      <Squares />
     </div>
   );
 }
