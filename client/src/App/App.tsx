3b44cd8ef59371483501053b846cb17ffd46aefd
Heisenberg <nancologist@gmail.com>
2021-02-26 11:30:32 +0100

    Add celebration annimation.

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -2,6 +2,7 @@ import { Fragment, useEffect, useState } from 'react';
 import AppForm from '../components/AppForm/AppForm';
 import Squares from '../components/squares/Squares';
 import { socket } from '../socket/socket';
+import { celebrationColors } from './data';
 import './App.css';
 
 function App() {
@@ -22,11 +23,19 @@ function App() {
     setGameStarted(true);
   }
 
+  const celebrate = () => {
+    celebrationColors.forEach((color, i) => {
+      setInterval(() => {
+        document.body.style.backgroundColor = color;
+      }, 300 * (i + 1));
+    })
+  };
+
   let content = <AppForm submitted={startGame} />
   if (gameStarted) {
     content = 
       <Fragment>
-        <Squares />
+        <Squares haveWinner={celebrate}/>
         <h3>You vs {
           oppName ? oppName:
           <span style={{ color: '#888'}}>waiting...</span>
@@ -35,7 +44,7 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <div className="App" >
       <h1>Vingo!</h1>
       { content }
     </div>
